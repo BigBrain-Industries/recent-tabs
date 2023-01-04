e992279220c51eb0648a1400c6650ac2e3bc3027
reihan <reihan@bigbrain-industries.com>
2023-01-04 11:14:42 -0800

    move tab when new tab created + added added more logging + support for Chrome

--- background.js
+++ background.js
@@ -6,32 +6,152 @@ function onError(error) {
     console.log(`Error: ${error}`);
 }
 
-const tabToTimeout = new Map();
+const tabIdToTimeout = new Map();
+const timeBeforeTabConsideredViewed = 3000;
 
-async function onActivatedListener(activeInfo) {
+function toString(tab) {
+    return `tab(${tab.id}) "${tab.title}" `
+}
+
+async function removeTimeout(tabId) {
+    await cancelTabMove(tabId);
+}
+
+async function moveTabToEnd(tabId) {
+    let tab = await browser.tabs.get(tabId);
+
+    // if (tab.active) {
+    let moving = browser.tabs.move(tabId, {index: -1});
+    console.log(`moving ${toString(tab)} to end`);
+    moving.then(onMoved, onError);
+    // }
+}
+
+async function onTabActivated(activeInfo) {
     let tabId = activeInfo.tabId;
     let tab = await browser.tabs.get(tabId);
 
-    // cancel any pending moves and start a new move
-    if (tabToTimeout.has(tabId)) {
-        let timeout = tabToTimeout.get(tabId);
-        clearTimeout(timeout);
-        console.log(`deleting existing timeout ${timeout} for tab ${tabId}`);
-        tabToTimeout.delete(tabId);
+    // cancel all other tabs timeouts
+    for (let [existingTabId] of tabIdToTimeout) {
+        if (tabId !== existingTabId) {
+            try {
+                let existingTab = await browser.tabs.get(existingTabId);
+                console.log(`tab ${toString(tab)} was activated. Removing existing timeout for tab ${toString(existingTab)}`)
+                await removeTimeout(existingTabId);
+            } catch (e) {
+                console.warn(`failed to remove tmieout for existing tab ${existingTabId}`);
+            }
+        }
+    }
+
+    if (tabIdToTimeout.has(tabId)) {
+        console.log(`tab ${toString(tab)} already has a move timeout. Cancelling that timeout.`);
+        await cancelTabMove(tabId);
     }
 
     let moveTimeoutId = setTimeout(async function () {
-        let tab = await browser.tabs.get(tabId);
-        console.log(`tab ${tabId} active state: ${tab.active}`);
-        if (tab.active) {
-            let moving = browser.tabs.move(tabId, {index: -1});
-            moving.then(onMoved, onError);
+        await moveTabToEnd(tabId);
+    }, timeBeforeTabConsideredViewed);
+    console.log(`setting timeout ${moveTimeoutId} for tab ${toString(tab)}`);
+    tabIdToTimeout.set(tabId, moveTimeoutId);
+}
+
+async function cancelTabMove(tabId) {
+
+    // cancel any pending moves
+    if (tabIdToTimeout.has(tabId)) {
+        let timeout = tabIdToTimeout.get(tabId);
+        clearTimeout(timeout);
+        try {
+            let tab = await browser.tabs.get(tabId);
+            console.log(`deleted existing timeout ${timeout} for tab ${tab}`);
+        } catch (e) {
+            // tab might not exist anymore
         }
-    }, 3000);
-    console.log(`setting timeout ${moveTimeoutId} for tab ${tabId}`);
-    tabToTimeout.set(tabId, moveTimeoutId);
+        tabIdToTimeout.delete(tabId);
+    }
+}
+
+async function onTabCreated(tab) {
+
+    // the created tab and parent tab should immediately move all the way to the right.
+    if (tab.openerTabId) {
+        let parentTab = await browser.tabs.get(tab.openerTabId);
+        console.log(`parent tab is ${parentTab.title}`);
+        const parentTabId = parentTab.id;
+        if (tabIdToTimeout.has(parentTabId)) {
+            await cancelTabMove(parentTabId);
+            await moveTabToEnd(parentTabId);
+            await moveTabToEnd(tab.id);
+        }
+    }
+
+}
+
+async function onTabRemoved(tabId) {
+    await cancelTabMove(tabId);
+}
+
+async function onTabMoved(tabId) {
+    // fires both when moved by user and when moved by this plugin
+    // await cancelTabMove(tabId);
+}
+
+async function onTabDetached(tabId) {
+    await cancelTabMove(tabId);
 }
 
 browser.tabs.onActivated.addListener(
-    onActivatedListener
+    async (activeInfo) => {
+        try {
+            await onTabActivated(activeInfo);
+        } catch (e) {
+            console.error(e);
+        }
+    }
 );
+
+browser.tabs.onRemoved.addListener(
+    async (tabId) => {
+        try {
+            console.info(`removing tab ${tabId}`);
+            await onTabRemoved(tabId);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+);
+
+browser.tabs.onMoved.addListener(
+    async (tabId) => {
+        try {
+            await onTabMoved(tabId);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+);
+
+browser.tabs.onDetached.addListener(
+    async (tabId) => {
+        try {
+            await onTabDetached(tabId);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+);
+
+browser.tabs.onCreated.addListener(
+    async (tab) => {
+        try {
+            await onTabCreated(tab);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+);
+
+browser.runtime.onSuspend.addListener(() => {
+    console.log("Unloading.");
+});
